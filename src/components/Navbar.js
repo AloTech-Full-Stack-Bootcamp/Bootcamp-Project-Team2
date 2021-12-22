057e35b8c2b5be7c3137b4752f141a0db2cfed4c
Oğuz GÜL <ogzgl06@gmail.com>
2021-12-22 19:23:52 +0300

    Initialize Project

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div>
+        <h2>Admin Panel</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
