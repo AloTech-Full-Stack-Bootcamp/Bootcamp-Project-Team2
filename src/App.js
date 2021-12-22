fd311ade24be6d87714dc4e53af05c6b8dc17aa7
Oğuz GÜL <ogzgl06@gmail.com>
2021-12-22 20:31:34 +0300

    Make route home pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-     
-      <Navbar/>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Navbar.css";
+import {  Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div>
+      <div className="navbar__header">
         <h2>Admin Panel</h2>
+      </div>
+
+      <div className="navbar__link">
+        <h4>
+          <Link className="text_link" to="/home">
+            Home
+          </Link>
+        </h4>
+        <h4>
+          <Link className="text_link" to="/process">
+            Process
+          </Link>
+        </h4>
       </div>
     </div>
   );
