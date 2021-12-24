fa2759b226acf574b2773cbf0ef41b2746cab35e
Oğuz GÜL <ogzgl06@gmail.com>
2021-12-24 18:10:18 +0300

    Config JWTToken on the frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,22 @@ import "./index.css";
 import "./static/css/bootstrap.min.css";
 import App from "./App";
 
+import { setAuthorizationToken } from "./helpers/setAuthorizationToken";
+
+//Redux config
+import { Provider } from "react-redux";
+import store from "./helpers/Store";
+
+const jwtToken = localStorage.getItem("jwtToken");
+if (jwtToken) {
+  setAuthorizationToken(jwtToken);
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { setAuthorizationToken } from "../helpers/setAuthorizationToken";
+
+export const login = (username, password) => {
+  return axios
+    .post("http://localhost:3000/account/login", { username, password })
+    .then((user) => {
+      if (user.data.status) {
+        const { token } = user.data;
+        localStorage.setItem("jwtToken", token);
+        setAuthorizationToken(token);
+      }
+      return user.data;
+    })
+    .catch((err) => console.log(err));
+};
+
+export const logout = () => {
+  localStorage.removeItem("jwtToken");
+  setAuthorizationToken(false);
+};
