4682009b99b1bde9def6274ec059262fbbdcccdb
Oğuz GÜL <ogzgl06@gmail.com>
2021-12-28 15:36:50 +0300

    Login Auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Process from "./components/Process";
 import Login from "./components/Login";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const [username, setUsername] = useState("");
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
-    <Router>
-      {!username ? (
+    <>
+      <LoginButton />
+      <LogoutButton />
+      {true ? (
         <Login />
       ) : (
-        <div className="app">
-          <Navbar />
+        <Router>
+          <div className="app">
+            <Navbar />
 
-          <Routes>
-            <Route path="/process" element={<Process />} />
-            <Route exact path="/" element={<Home />} />
-          </Routes>
-        </div>
+            <Routes>
+              <Route path="/process" element={<Process />} />
+              <Route exact path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </Router>
       )}
-    </Router>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./static/css/bootstrap.min.css";
 import App from "./App";
-
 import { setAuthorizationToken } from "./helpers/setAuthorizationToken";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 //Redux config
 import { Provider } from "react-redux";
@@ -15,11 +15,16 @@ if (jwtToken) {
   setAuthorizationToken(jwtToken);
 }
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.login.origin}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
